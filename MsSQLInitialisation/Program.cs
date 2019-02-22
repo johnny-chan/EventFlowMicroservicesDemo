2287c86cdc55159421be3bef6a5e7a60a4e6badf
Johnny Chan <jcwc1@hotmail.com>
2019-02-22 15:17:51 +0000

    convert console app to use user secrets

--- MsSQLInitialisation/Program.cs
+++ MsSQLInitialisation/Program.cs
@@ -1,13 +1,21 @@
-﻿using EventFlow;
+﻿using System;
+using System.IO;
+using Common.Settings;
+using EventFlow;
 using EventFlow.AspNetCore.Extensions;
 using EventFlow.MsSql;
 using EventFlow.MsSql.EventStores;
 using EventFlow.MsSql.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration.UserSecrets;
 
 namespace MsSQLInitialisation
 {
     class Program
     {
+        public static IConfigurationRoot Configuration { get; set; }
+
         /// <summary>
         /// Initalises the eventflow database tables
         /// </summary>
@@ -15,11 +23,34 @@ namespace MsSQLInitialisation
         /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var devEnvironmentVariable = Environment.GetEnvironmentVariable("NETCORE_ENVIRONMENT");
+            var isDevelopment = string.IsNullOrEmpty(devEnvironmentVariable) || devEnvironmentVariable.ToLower() == "development";
+            //Determines the working environment as IHostingEnvironment is unavailable in a console app
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory());
+               
+
+            if (isDevelopment) //only add secrets in development
+            {
+                builder.AddUserSecrets("babaea2e-4e26-4c24-8243-7f166cdf3230");
+            }
+
+            Configuration = builder.Build();
+
+            var connectionString = Configuration["EventStoreConnectionString"];
+
+            //var services = new ServiceCollection()
+            //    .Configure<Secrets>(Configuration.GetSection(nameof(Secrets)))
+            //    .AddOptions()
+            //    .BuildServiceProvider();
+
+            //services.GetService<Secrets>();
+
             var rootResolver = EventFlowOptions.New
                 .AddAspNetCoreMetadataProviders()
                 .UseMssqlEventStore()
-                .ConfigureMsSql(MsSqlConfiguration.New.SetConnectionString(
-                    "Data Source=JC_MSI;Initial Catalog=EventFlowDemo;Integrated Security=True;Persist Security Info=False;Pooling=False;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=True"))
+                .ConfigureMsSql(MsSqlConfiguration.New.SetConnectionString(connectionString))
                 .CreateResolver();
 
             var msSqlDatabaseMigrator = rootResolver.Resolve<IMsSqlDatabaseMigrator>();
